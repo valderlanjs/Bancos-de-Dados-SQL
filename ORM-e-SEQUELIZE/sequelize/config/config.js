855ceff0ca7c1d0e998810d1be4b2b52dc31ff7b
Valderlan <valderlanjosr15@gmail.com>
2024-05-21 23:47:35 -0300

    criado banco de dados pelo sequelize

---
+++ ORM-e-SEQUELIZE/sequelize/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "val2110#",
+    database: 'sequelize',
+    define: {
+        timestamps: true,
+    },
+};
